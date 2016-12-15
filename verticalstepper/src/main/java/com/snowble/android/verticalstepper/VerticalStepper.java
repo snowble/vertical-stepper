923d000faa40f8bdd1a08d189bab94780c8f74fe
Noel Mansour <noel.mansour@gmail.com>
2016-12-15 15:19:02 -0500

    Add constraintlayout dependency

--- verticalstepper/src/main/java/com/snowble/android/verticalstepper/VerticalStepper.java
+++ verticalstepper/src/main/java/com/snowble/android/verticalstepper/VerticalStepper.java
@@ -1,12 +1,10 @@
 package com.snowble.android.verticalstepper;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
+import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
 
-public class VerticalStepper extends ViewGroup {
+public class VerticalStepper extends ConstraintLayout {
     public VerticalStepper(Context context) {
         super(context);
     }
@@ -17,14 +15,5 @@ public class VerticalStepper extends ViewGroup {
 
     public VerticalStepper(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public VerticalStepper(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
-
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
     }
 }
