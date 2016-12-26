b0fa449d6ab33dae4792a034d33cebd4458b653b
Noel Mansour <327631+noelmansour@users.noreply.github.com>
2016-12-25 23:59:50 -0500

    Add test for toggle functionality
    - Add robolectric test dependency
    - Add mockito test dependency
    - Add assertj test dependencies

---
+++ verticalstepper/src/test/java/com/snowble/android/verticalstepper/VerticalStepperTest.java
@@ -0,0 +1,65 @@
+package com.snowble.android.verticalstepper;
+
+import android.app.Activity;
+import android.os.Build;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.M)
+public class VerticalStepperTest {
+
+    private Activity activity;
+    private VerticalStepper stepper;
+
+    @Before
+    public void before() {
+        ActivityController<DummyActivity> activityController = Robolectric.buildActivity(DummyActivity.class);
+        activity = activityController.create().get();
+        stepper = new VerticalStepper(activity);
+    }
+
+    @Test
+    public void toggleStepActiveState_ShouldToggleState() {
+        VerticalStepper.LayoutParams lp = mockLayoutParams();
+        lp.active = false;
+        when(lp.continueButton.getVisibility()).thenReturn(View.GONE);
+
+        View innerView = mock(View.class);
+        when(innerView.getVisibility()).thenReturn(View.GONE);
+        when(innerView.getLayoutParams()).thenReturn(lp);
+
+        stepper.toggleStepExpandedState(innerView);
+
+        assertThat(lp.active).isTrue();
+        verify(innerView).setVisibility(View.VISIBLE);
+        verify(lp.continueButton).setVisibility(View.VISIBLE);
+    }
+
+    private VerticalStepper.LayoutParams mockLayoutParams() {
+        Robolectric.AttributeSetBuilder attributeSetBuilder = Robolectric.buildAttributeSet();
+        attributeSetBuilder.addAttribute(android.R.attr.layout_width, "wrap_content");
+        attributeSetBuilder.addAttribute(android.R.attr.layout_height, "wrap_content");
+
+        attributeSetBuilder.addAttribute(R.attr.step_title, "title");
+        VerticalStepper.LayoutParams lp = new VerticalStepper.LayoutParams(activity, attributeSetBuilder.build());
+        lp.continueButton = mock(AppCompatButton.class);
+        return lp;
+    }
+
+    private static class DummyActivity extends Activity {
+    }
+}
