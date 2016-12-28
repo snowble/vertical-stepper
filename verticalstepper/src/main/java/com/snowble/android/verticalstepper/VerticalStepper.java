69c47b3a7ff8dffc8597d0bb5c4c0baad7d6e2b2
Noel Mansour <327631+noelmansour@users.noreply.github.com>
2016-12-28 14:02:21 -0500

    Avoid calling android methods from tests
    The android method/callback is not indicative of the actual
    functionality. Add explicit methods that are indicative.

--- verticalstepper/src/main/java/com/snowble/android/verticalstepper/VerticalStepper.java
+++ verticalstepper/src/main/java/com/snowble/android/verticalstepper/VerticalStepper.java
@@ -271,6 +271,11 @@ public class VerticalStepper extends ViewGroup {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+        initChildViews();
+    }
+
+    @VisibleForTesting
+    void initChildViews() {
         int childCount = getChildCount();
         for (int i = 0; i < childCount; i++) {
             initInnerView(getChildAt(i));
