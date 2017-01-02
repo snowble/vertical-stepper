2508f948014f26ce4dc1897b7ec5312057b4bb84
Noel Mansour <noel.mansour@gmail.com>
2017-01-02 16:38:02 -0500

    Switch to hierarchical tests for StepTest

--- verticalstepper/src/test/java/com/snowble/android/verticalstepper/StepTest.java
+++ verticalstepper/src/test/java/com/snowble/android/verticalstepper/StepTest.java
@@ -9,6 +9,7 @@ import android.view.View;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
@@ -18,11 +19,8 @@ import org.robolectric.util.ActivityController;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.M)
+@RunWith(Enclosed.class)
 public class StepTest {
-    private VerticalStepper.Step step;
-
     private static final TextPaint TITLE_ACTIVE_PAINT = new TextPaint();
     private static final TextPaint TITLE_INACTIVE_PAINT = new TextPaint();
     private static final Paint ICON_ACTIVE_PAINT = new TextPaint();
@@ -30,84 +28,96 @@ public class StepTest {
     private static final int ACTIVE_BOTTOM_MARGIN = 48;
     private static final int INACTIVE_BOTTOM_MARGIN = 40;
 
-    @Before
-    public void before() {
-        VerticalStepper.Step.setTitleActiveTextPaint(TITLE_ACTIVE_PAINT);
-        VerticalStepper.Step.setTitleInactiveTextPaint(TITLE_INACTIVE_PAINT);
-        VerticalStepper.Step.setIconActiveBackgroundPaint(ICON_ACTIVE_PAINT);
-        VerticalStepper.Step.setIconInactiveBackgroundPaint(ICON_INACTIVE_PAINT);
-        VerticalStepper.Step.setActiveBottomMarginToNextStep(ACTIVE_BOTTOM_MARGIN);
-        VerticalStepper.Step.setInactiveBottomMarginToNextStep(INACTIVE_BOTTOM_MARGIN);
+    @RunWith(RobolectricTestRunner.class)
+    @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.M)
+    public abstract static class GivenAStep {
+        protected VerticalStepper.Step step;
 
-        ActivityController<Activity> activityController = Robolectric.buildActivity(Activity.class);
-        Activity activity = activityController.create().get();
-        View innerView = mock(View.class);
-        when(innerView.getLayoutParams()).thenReturn(RobolectricTestUtils.createTestLayoutParams(activity));
+        @Before
+        public void givenAStep() {
+            VerticalStepper.Step.setTitleActiveTextPaint(TITLE_ACTIVE_PAINT);
+            VerticalStepper.Step.setTitleInactiveTextPaint(TITLE_INACTIVE_PAINT);
+            VerticalStepper.Step.setIconActiveBackgroundPaint(ICON_ACTIVE_PAINT);
+            VerticalStepper.Step.setIconInactiveBackgroundPaint(ICON_INACTIVE_PAINT);
+            VerticalStepper.Step.setActiveBottomMarginToNextStep(ACTIVE_BOTTOM_MARGIN);
+            VerticalStepper.Step.setInactiveBottomMarginToNextStep(INACTIVE_BOTTOM_MARGIN);
 
-        step = new VerticalStepper.Step(innerView,
-                new VerticalStepper.InternalTouchView(activity), new AppCompatButton(activity));
+            ActivityController<Activity> activityController = Robolectric.buildActivity(Activity.class);
+            Activity activity = activityController.create().get();
+            View innerView = mock(View.class);
+            when(innerView.getLayoutParams()).thenReturn(RobolectricTestUtils.createTestLayoutParams(activity));
+
+            step = new VerticalStepper.Step(innerView,
+                    new VerticalStepper.InternalTouchView(activity), new AppCompatButton(activity));
+        }
     }
 
-    @Test
-    public void getIconColor_InactiveStep_ShouldReturnInactiveStepPaint() {
-        step.setActive(false);
+    public static class GivenStepIsTheLastOne extends GivenAStep {
+        @Test
+        public void getBottomMarginToNextStep_ShouldReturnZeroSizedMargin() {
+            int margin = step.getBottomMarginToNextStep(true);
 
-        Paint paint = step.getIconColor();
-
-        assertThat(paint).isSameAs(ICON_INACTIVE_PAINT);
+            assertThat(margin).isEqualTo(VerticalStepper.Step.ZERO_SIZE_MARGIN);
+        }
     }
 
-    @Test
-    public void getIconColor_ActiveStep_ShouldReturnInactiveStepPaint() {
-        step.setActive(true);
+    public static class GivenStepIsInactive extends GivenAStep {
+        @Before
+        public void givenStepIsInactive() {
+            step.setActive(false);
+        }
 
-        Paint paint = step.getIconColor();
+        @Test
+        public void getIconColor_ShouldReturnInactiveStepPaint() {
+            Paint paint = step.getIconColor();
 
-        assertThat(paint).isSameAs(ICON_ACTIVE_PAINT);
+            assertThat(paint).isSameAs(ICON_INACTIVE_PAINT);
+        }
+
+        @Test
+        public void getTitleTextPaint_ShouldReturnInactiveStepPaint() {
+            TextPaint paint = step.getTitleTextPaint();
+
+            assertThat(paint).isSameAs(TITLE_INACTIVE_PAINT);
+        }
     }
 
-    @Test
-    public void getTitleTextPaint_InactiveStep_ShouldReturnInactiveStepPaint() {
-        step.setActive(false);
+    public static class GivenStepIsInactiveAndInTheMiddle extends GivenStepIsInactive {
+        @Test
+        public void getBottomMarginToNextStep_ShouldReturnInactiveMargin() {
+            int margin = step.getBottomMarginToNextStep(false);
 
-        TextPaint paint = step.getTitleTextPaint();
-
-        assertThat(paint).isSameAs(TITLE_INACTIVE_PAINT);
+            assertThat(margin).isEqualTo(INACTIVE_BOTTOM_MARGIN);
+        }
     }
 
-    @Test
-    public void getTitleTextPaint_ActiveStep_ShouldReturnActiveStepPaint() {
-        step.setActive(true);
+    public static class GivenStepIsActive extends GivenAStep {
+        @Before
+        public void givenStepIsActive() {
+            step.setActive(true);
+        }
 
-        TextPaint paint = step.getTitleTextPaint();
+        @Test
+        public void getIconColor_ShouldReturnInactiveStepPaint() {
+            Paint paint = step.getIconColor();
 
-        assertThat(paint).isSameAs(TITLE_ACTIVE_PAINT);
+            assertThat(paint).isSameAs(ICON_ACTIVE_PAINT);
+        }
+
+        @Test
+        public void getTitleTextPaint_ShouldReturnActiveStepPaint() {
+            TextPaint paint = step.getTitleTextPaint();
+
+            assertThat(paint).isSameAs(TITLE_ACTIVE_PAINT);
+        }
     }
 
-    @Test
-    public void getBottomMarginToNextStep_LastStep_ShouldReturnZeroSizedMargin() {
-        step.setActive(false);
+    public static class GivenStepIsActiveAndInTheMiddle extends GivenStepIsActive {
+        @Test
+        public void getBottomMarginToNextStep_ShouldReturnActiveMargin() {
+            int margin = step.getBottomMarginToNextStep(false);
 
-        int margin = step.getBottomMarginToNextStep(true);
-
-        assertThat(margin).isEqualTo(VerticalStepper.Step.ZERO_SIZE_MARGIN);
-    }
-
-    @Test
-    public void getBottomMarginToNextStep_NotLastStepInactive_ShouldReturnInactiveMargin() {
-        step.setActive(false);
-
-        int margin = step.getBottomMarginToNextStep(false);
-
-        assertThat(margin).isEqualTo(INACTIVE_BOTTOM_MARGIN);
-    }
-
-    @Test
-    public void getBottomMarginToNextStep_NotLastStepActive_ShouldReturnActiveMargin() {
-        step.setActive(true);
-
-        int margin = step.getBottomMarginToNextStep(false);
-
-        assertThat(margin).isEqualTo(ACTIVE_BOTTOM_MARGIN);
+            assertThat(margin).isEqualTo(ACTIVE_BOTTOM_MARGIN);
+        }
     }
 }
