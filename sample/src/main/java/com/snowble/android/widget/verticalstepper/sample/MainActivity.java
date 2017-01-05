4107a525b63ec7db01bf2aec10aa862cc20c9238
Noel Mansour <327631+noelmansour@users.noreply.github.com>
2017-01-05 14:10:39 -0500

    Update package to mirror android's pkging style

--- sample/src/main/java/com/snowble/android/widget/verticalstepper/sample/MainActivity.java
+++ sample/src/main/java/com/snowble/android/widget/verticalstepper/sample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.snowble.android.verticalstepper.sample;
+package com.snowble.android.widget.verticalstepper.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- verticalstepper/src/test/java/com/snowble/android/widget/verticalstepper/VerticalStepperTest.java
+++ verticalstepper/src/test/java/com/snowble/android/widget/verticalstepper/VerticalStepperTest.java
@@ -1,4 +1,4 @@
-package com.snowble.android.verticalstepper;
+package com.snowble.android.widget.verticalstepper;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
