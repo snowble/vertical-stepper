db0d60528d0c53a753228b48a5466b620a902b8e
Noel Mansour <327631+noelmansour@users.noreply.github.com>
2017-01-11 15:00:55 -0500

    Add tests for ViewUtils

---
+++ verticalstepper/src/test/java/com/snowble/android/widget/verticalstepper/ViewUtilsTest.java
@@ -0,0 +1,51 @@
+package com.snowble.android.widget.verticalstepper;
+
+import android.graphics.Rect;
+import android.text.TextPaint;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class ViewUtilsTest {
+    private String text;
+    private TextPaint paint;
+
+    @Before
+    public void setUp() {
+        text = "vertical stepper";
+        paint = mock(TextPaint.class);
+    }
+
+    @Test
+    public void findTextCenterStartX() throws Exception {
+        float textWidth = 30f;
+        int totalWidth = 100;
+        when(paint.measureText(text)).thenReturn(textWidth);
+
+        float centerX = ViewUtils.findTextCenterStartX(text, totalWidth, paint);
+
+        Rect outer = new Rect(0, 0, totalWidth, 0);
+        Rect inner = new Rect(0, 0, (int) textWidth, 0);
+        int expectedCenter = outer.centerX() - inner.centerX();
+        assertThat(centerX).isEqualTo(expectedCenter);
+    }
+
+    @Test
+    public void findTextCenterStartY() throws Exception {
+        int textHeight = 30;
+        int totalHeight = 100;
+        Rect measureRect = new Rect(0, 0, 0, textHeight);
+
+        float centerY = ViewUtils.findTextCenterStartY(text, totalHeight, paint, measureRect);
+
+        verify(paint).getTextBounds(text, 0, 1, measureRect);
+
+        Rect outer = new Rect(0, 0, 0, totalHeight);
+        Rect inner = new Rect(0, 0, 0, textHeight);
+        int expectedCenter = outer.centerY() + inner.centerY();
+        assertThat(centerY).isEqualTo(expectedCenter);
+    }
+}
